6026ea195e552aea90fa3669727e53bdefc8482a
xodnjs0876 <taewon.kim@wavle.io>
2023-08-25 18:41:31 +0900

    라우터 연결

--- publishing/src/App.tsx
+++ publishing/src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { styled } from 'styled-components';
+import NotisDetail from './screen/notis/detail/notisDetail';
+import Notification from './screen/notis/notification';
+import { Route, Routes } from 'react-router-dom';
+import MainLayout from './components/mainLayout';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Routes>
+            <Route Component={MainLayout}>
+              <Route path="/" Component={Notification} />
+              <Route path='/post/:id' Component={NotisDetail} />
+            </Route>
+      </Routes>
+    </Layout>
   );
 }
 
-export default App;
+const Layout = styled.div`
+  body {
+    margin:0px;
+    padding:0px;
+  }
+`
