5f6caf4a5b46e5afa8842f5c64889df0732a1fc9
xodnjs0876 <taewon.kim@wavle.io>
2023-08-25 18:39:11 +0900

    라우터 메인레이아웃

---
+++ publishing/src/components/mainLayout.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../screen/header/header';
+import Footer from '../screen/footer/footer';
+import { Outlet } from 'react-router-dom';
+
+export default function MainLayout() {
+    return (
+        <>
+            <Header/>
+            <Outlet/>
+            <Footer/>
+        </>
+    )
+}
