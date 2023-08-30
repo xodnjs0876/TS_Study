5b584695aa77bbe6abb000c2c985081a5130da85
xodnjs0876 <taewon.kim@wavle.io>
2023-08-30 18:59:15 +0900

    공지사항 상세 페이지 쿼리

---
+++ publishing/src/graphql/notice/query/notice-post.tsx
@@ -0,0 +1,33 @@
+import { gql } from "@apollo/client";
+
+const GET_POST_DATA = gql`
+query NoticePost($noticePostId: ID!) {
+    noticePost(id: $noticePostId) {
+        viewCnt
+        title
+        replyCount
+        likeCnt
+        isLike
+        isFavorite
+        id
+        content
+        files {
+            id
+            url
+            filename
+        }
+        createdAt
+        category {
+            name
+            id
+        }
+        author {
+            name
+            nickname
+            id
+        }
+    }
+}
+`;
+
+export default GET_POST_DATA;
