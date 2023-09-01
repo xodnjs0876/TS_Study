a23e2f891cf7775116c0f03148dcb05f56119a59
xodnjs0876 <taewon.kim@wavle.io>
2023-09-01 17:55:04 +0900

    카카오 access token mutation

---
+++ publishing/src/graphql/notice/mutation/get-kakao-access-token.tsx
@@ -0,0 +1,14 @@
+import { gql } from "@apollo/client";
+
+const MUTATION_GET_KAKAO_ACCESS_TOKEN = gql`
+mutation GetKakaoAccessToken($code: String!, $redirectUri: String!) {
+    getKakaoAccessToken(code: $code, redirectUri: $redirectUri) {
+        isJoin
+        jwtData {
+            access_token
+        }
+        }
+    }
+`;
+
+export default MUTATION_GET_KAKAO_ACCESS_TOKEN;
