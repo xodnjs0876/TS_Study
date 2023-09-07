4638aac89bb67fa88405d22a14fa794dff1076b5
xodnjs0876 <taewon.kim@wavle.io>
2023-09-07 13:41:32 +0900

    Provider 추가

--- publishing/src/App.tsx
+++ publishing/src/App.tsx
@@ -4,17 +4,29 @@ import NotisDetail from './screen/notis/detail/notisDetail';
 import Notification from './screen/notis/notification';
 import { Route, Routes } from 'react-router-dom';
 import MainLayout from './components/mainLayout';
+import Login from './screen/auth/login';
+import KaKaoRedirect from './screen/auth/kakao';
+import { AuthProvider } from './components/auth/provider';
+import { CookiesProvider } from 'react-cookie';
+import NaverRedirect from './screen/auth/naver';
 
 export default function App() {
   return (
-    <Layout>
-      <Routes>
-            <Route Component={MainLayout}>
-              <Route path="/" Component={Notification} />
-              <Route path='/post/:id' Component={NotisDetail} />
-            </Route>
-      </Routes>
-    </Layout>
+    <CookiesProvider>
+      <AuthProvider>
+        <Layout>
+          <Routes>
+              <Route Component={MainLayout}>
+                <Route path="/" Component={Notification} />
+                <Route path='/post/:id' Component={NotisDetail} />
+                <Route path='/oauth/' Component={Login}/>
+              </Route>
+              <Route path='/oauth/kakao' Component={KaKaoRedirect}/> 
+              <Route path='/oauth/naver' Component={NaverRedirect}/>
+          </Routes>
+        </Layout>
+        </AuthProvider>
+      </CookiesProvider>
   );
 }
 
--- publishing/src/components/auth/provider.tsx
+++ publishing/src/components/auth/provider.tsx
@@ -1,28 +1,33 @@
-import React, { useContext, useLayoutEffect, useState } from "react";
-import { getCookie } from "../cookie";
+import React, { useCallback, useContext, useState } from "react";
 
-export const IsLoggedIn = React.createContext(false);
+export const IsLoggedIn = React.createContext<[Boolean,(check:string)=>void,()=>void]>([false,(check:string)=>{},()=>{}]);
 export function useIsLoggedInContext() {
   return useContext(IsLoggedIn);
-}
-
-export function useIsLoggin() {
-    return React.useContext(IsLoggedIn);
 }
 
 export const AuthProvider = ({ children }: React.PropsWithChildren) => {
 
     const [isLoggedIn, setLoggedIn] = useState<boolean>(false);
+    const setLogin=useCallback((access_token:string)=>{
+        setLoggedIn(true);
+        window.sessionStorage.setItem("accessToken",access_token)
+    },[setLoggedIn])
 
-    useLayoutEffect(() => {
-        const isLoggedIn = getCookie("token");
-        if (isLoggedIn) {
-            setLoggedIn(true);
-        } else {
-            setLoggedIn(false);
-        }
-        return;
-        }, []);
+    const logout = useCallback(()=>{
+        setLoggedIn(false);
+        window.sessionStorage.clear();
+        window.location.reload();
+    },[setLoggedIn])
 
-    return <IsLoggedIn.Provider value={isLoggedIn}>{children}</IsLoggedIn.Provider>;
+    // useLayoutEffect(() => {
+
+    //     if (isLoggedIn) {
+    //         setLoggedIn(true);
+    //     } else {
+    //         setLoggedIn(false);
+    //     }
+    //     return;
+    //     }, []);
+
+    return <IsLoggedIn.Provider value={[isLoggedIn,setLogin,logout]}>{children}</IsLoggedIn.Provider>;
 };
