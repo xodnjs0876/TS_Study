a4a11fbe25b9ea8bdc43734f317ab937782b91cc
xodnjs0876 <taewon.kim@wavle.io>
2023-09-07 14:55:44 +0900

    네이버 Redirect

--- publishing/src/screen/auth/naver.tsx
+++ publishing/src/screen/auth/naver.tsx
@@ -4,6 +4,7 @@ import { useLocation, useNavigate } from "react-router-dom";
 import MUTATION_NAVER_SIGN_UP from "../../graphql/notice/mutation/naver-sign-up";
 import { error } from "console";
 import { getCookie, removeCookie, setCookie } from "../../components/cookie";
+import { useIsLoggedInContext } from "../../components/auth/provider";
 
 export default function NaverRedirect() {
 
@@ -11,6 +12,7 @@ export default function NaverRedirect() {
     const location = useLocation();
     const code = new URL(window.location.href).searchParams.get("code")!;
     const state = new URL(window.location.href).searchParams.get("state")!;
+    const [isLoggin,setLogin,logout] = useIsLoggedInContext();
     const [accessToken] = useMutation(MUTATION_NAVER_SIGN_UP);
 
     useEffect(()=> {
@@ -25,8 +27,10 @@ export default function NaverRedirect() {
                 })
                 .then(res => {
                     if(res.data.signUpNaver.user.id){
-                        // navigate("/")
-                        navigate(0)
+                        setLogin(`${res.data.signUpNaver.token.accessToken}`)
+                        navigate("/", {
+                            replace: true,
+                        })
                     }
                     else {
                         console.log("에러")
@@ -39,38 +43,33 @@ export default function NaverRedirect() {
                 // removeCookie("token", {
                 //     path: '/',
                 // });
-                // navigate("/oauth")
+                navigate("/oauth")
             } 
         });
     })
 
-    const getNaverToken = () :Promise<string> => {
-        
+    const getNaverToken = () : Promise<string> => {
         return new Promise((resovle,reject)=>{
-            console.log(`https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id=${process.env.REACT_APP_NAVER_API_KEY}&client_secret=${process.env.REACT_APP_NAVER_SECRET_KEY}&code=${code}&state=${state}`)
-            resovle("")
-        //     fetch(`https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id=${process.env.REACT_APP_NAVER_API_KEY}&client_secret=${process.env.REACT_APP_NAVER_SECRET_KEY}&code=${code}&state=${state}`, {
-        //         method: "GET",
-        //         // headers: {
-        //         //     "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
-        //         // },
-        //         // body: ``,
-        // })
-        // .then(res => res.json())
-        // .then(data => {
-        //     console.log("data",data);
-            
-        //     if(data.access_token) {
-        //         resovle(data.access_token)
-        //     } else {
-        //         console.log("나는 토큰 데이타", data)
-        //         reject(new Error("나는 토큰 데이타"))
-        //     }
-        // })
-        // .catch(error => {
-        //     console.log("에러",error);
-        //     reject(error)
-        // })
+            fetch(`/oauth2.0/token?grant_type=authorization_code&client_id=${process.env.REACT_APP_NAVER_API_KEY}&client_secret=${process.env.REACT_APP_NAVER_SECRET_KEY}&code=${code}&state=${state}`, {
+                method: "GET",
+                // headers: {
+                //     "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
+                // },
+                // body: `grant_type=authorization_code&client_id=${process.env.REACT_APP_NAVER_API_KEY}&client_secret=${process.env.REACT_APP_NAVER_SECRET_KEY}&code=${code}&state=${state}`,
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data) {
+                resovle(data.access_token)
+            } else {
+                console.log("나는 토큰 데이타", data)
+                reject(new Error("나는 토큰 데이타"))
+            }
+        })
+        .catch(error => {
+            console.log("에러",error);
+            reject(error)
+        })
         })
     };
 
