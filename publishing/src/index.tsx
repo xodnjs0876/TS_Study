47c1b487d2430b96037da1a0d901cbfd985c5ad5
xodnjs0876 <taewon.kim@wavle.io>
2023-09-08 13:19:48 +0900

    Apollo 서버 설정

--- publishing/src/index.tsx
+++ publishing/src/index.tsx
@@ -1,16 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+export const client = new ApolloClient({
+  uri: process.env.REACT_APP_BASE_URL,
+  cache: new InMemoryCache({}),
+});
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
+const googleClientId = process.env.REACT_APP_GOOGLE_CLIENT_ID ?? "";
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ApolloProvider client={client}>
+    <GoogleOAuthProvider clientId={googleClientId}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </GoogleOAuthProvider>
+  </ApolloProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
