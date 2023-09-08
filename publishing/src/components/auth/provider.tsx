b9aaf8d866f3d3423f82bbfca80e6fd65e9bd8eb
xodnjs0876 <taewon.kim@wavle.io>
2023-09-08 17:26:52 +0900

    Auth Provider 구현

--- publishing/src/components/auth/provider.tsx
+++ publishing/src/components/auth/provider.tsx
@@ -1,33 +1,43 @@
-import React, { useCallback, useContext, useState } from "react";
+import React, {
+  useCallback,
+  useContext,
+  useLayoutEffect,
+  useMemo,
+  useState,
+} from "react";
 
-export const IsLoggedIn = React.createContext<[Boolean,(check:string)=>void,()=>void]>([false,(check:string)=>{},()=>{}]);
+export const IsLoggedIn = React.createContext<
+  [Boolean, (check: string) => void, () => void]
+>([false, (check: string) => {}, () => {}]);
 export function useIsLoggedInContext() {
   return useContext(IsLoggedIn);
 }
 
 export const AuthProvider = ({ children }: React.PropsWithChildren) => {
+  const [isLoggedIn, setLoggedIn] = useState<boolean>(false);
+  const setLogin = useCallback(
+    (access_token: string) => {
+      setLoggedIn(true);
+      window.sessionStorage.setItem("accessToken", access_token);
+    },
+    [setLoggedIn]
+  );
 
-    const [isLoggedIn, setLoggedIn] = useState<boolean>(false);
-    const setLogin=useCallback((access_token:string)=>{
-        setLoggedIn(true);
-        window.sessionStorage.setItem("accessToken",access_token)
-    },[setLoggedIn])
+  const logout = useCallback(() => {
+    setLoggedIn(false);
+    window.sessionStorage.clear();
+  }, [setLoggedIn]);
 
-    const logout = useCallback(()=>{
-        setLoggedIn(false);
-        window.sessionStorage.clear();
-        window.location.reload();
-    },[setLoggedIn])
-
-    // useLayoutEffect(() => {
-
-    //     if (isLoggedIn) {
-    //         setLoggedIn(true);
-    //     } else {
-    //         setLoggedIn(false);
-    //     }
-    //     return;
-    //     }, []);
-
-    return <IsLoggedIn.Provider value={[isLoggedIn,setLogin,logout]}>{children}</IsLoggedIn.Provider>;
+  useLayoutEffect(() => {
+    if (window.sessionStorage.getItem("accessToken")) {
+      setLoggedIn(true);
+    } else {
+      setLoggedIn(false);
+    }
+  }, []);
+  return (
+    <IsLoggedIn.Provider value={[isLoggedIn, setLogin, logout]}>
+      {children}
+    </IsLoggedIn.Provider>
+  );
 };
