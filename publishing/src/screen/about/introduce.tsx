addf12c4e4a2d0f421bf06c153e22166161034ac
xodnjs0876 <taewon.kim@wavle.io>
2023-09-14 18:01:04 +0900

    한외협 퍼블리싱

---
+++ publishing/src/screen/about/introduce.tsx
@@ -0,0 +1,438 @@
+export default function Introduce() {
+  return (
+    <div style={{ margin: "auto", maxWidth: "1240px" }}>
+      <p
+        style={{
+          textAlign: "center",
+          color: "#333",
+          fontSize: "30px",
+          lineHeight: "44px",
+          fontFamily: "Pretendard",
+          fontWeight: 700,
+          paddingBottom: "60px",
+        }}
+      >
+        MISSION
+      </p>
+      <p
+        style={{
+          textAlign: "center",
+          color: "#555",
+          fontSize: "30px",
+          lineHeight: "44px",
+          fontFamily: "Pretendard",
+          fontWeight: 400,
+        }}
+      >
+        한국외식산업연구원은 한국외식산업의 발전에 기여하고자,
+      </p>
+      <p style={{ textAlign: "center" }}>
+        <span
+          style={{
+            color: " #555",
+            fontSize: "30px",
+            lineHeight: "44px",
+            fontFamily: "Pretendard",
+            fontWeight: 400,
+          }}
+        >
+          산업에 관한 정보제공 및 연구기능에 중추적 역할을 담당한다.
+        </span>
+      </p>
+      <div
+        style={{
+          margin: "100px 0",
+          borderBottom: "1px solid #D8DDE5",
+        }}
+      ></div>
+      <p
+        style={{
+          textAlign: "center",
+          color: "#333",
+          fontSize: "30px",
+          lineHeight: "44px",
+          fontFamily: "Pretendard",
+          fontWeight: 700,
+          paddingBottom: "60px",
+        }}
+      >
+        VISION
+      </p>
+      <div
+        style={{
+          display: "flex",
+          gap: "30px",
+          flexDirection: "column",
+          margin: "auto",
+          width: "590px",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: "23px",
+          }}
+        >
+          <span
+            style={{
+              width: "20px",
+              height: "20px",
+              background: "#FFB800",
+              borderRadius: "100px",
+              flex: "0 0 20px",
+            }}
+          ></span>
+          <p
+            style={{
+              color: "#555",
+              fontSize: "20px",
+              lineHeight: "28px",
+              fontFamily: "Pretendard",
+              fontWeight: 400,
+            }}
+          >
+            외식산업종사자의 소득 및 경영안정과 복지증진을 위한 정보제공
+          </p>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: "23px",
+          }}
+        >
+          <span
+            style={{
+              width: "20px",
+              height: "20px",
+              background: "#60D165",
+              borderRadius: "100px",
+              flex: "0 0 20px",
+            }}
+          ></span>
+          <p
+            style={{
+              color: "#555",
+              fontSize: "20px",
+              lineHeight: "28px",
+              fontFamily: "Pretendard",
+              fontWeight: 400,
+            }}
+          >
+            외신산업의 경쟁력 향상과 관련 산업의 수익성 기여
+          </p>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            gap: "23px",
+          }}
+        >
+          <span
+            style={{
+              width: "20px",
+              height: "20px",
+              background: "#0066FF",
+              borderRadius: "100px",
+              flex: "0 0 20px",
+              marginTop: "4px",
+            }}
+          ></span>
+          <p
+            style={{
+              color: "#555",
+              fontSize: "20px",
+              lineHeight: "28px",
+              fontFamily: "Pretendard",
+              fontWeight: 400,
+            }}
+          >
+            외식•식품가공산업인적자원 개발 및 교육훈련의 미스매칭 해소를 위한
+            직업능력 개발 시스템 개편 주도
+          </p>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: "23px",
+          }}
+        >
+          <span
+            style={{
+              width: "20px",
+              height: "20px",
+              background: "#624EDE",
+              borderRadius: "100px",
+              flex: "0 0 20px",
+            }}
+          ></span>
+          <p
+            style={{
+              color: "#555",
+              fontSize: "20px",
+              lineHeight: "28px",
+              fontFamily: "Pretendard",
+              fontWeight: 400,
+            }}
+          >
+            외식산업 내 관련 협력체 간 중추적 가교 역할 수행
+          </p>
+        </div>
+      </div>
+      <div
+        style={{
+          margin: "100px 0",
+          borderBottom: "1px solid #D8DDE5",
+        }}
+      ></div>
+      <p
+        style={{
+          textAlign: "center",
+          color: " #333",
+          fontSize: "30px",
+          lineHeight: "44px",
+          fontFamily: "Pretendard",
+          fontWeight: 700,
+          paddingBottom: "60px",
+        }}
+      >
+        BUSINESS FIELD
+      </p>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(3, minmax(0, 200px))",
+          gap: "10px",
+          justifyContent: "center",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            boxSizing: "border-box",
+            justifyContent: "center",
+            alignItems: "center",
+            width: "200px",
+            height: "200px",
+            borderRadius: "100px",
+            border: "1px solid #D8DDE5",
+            background: "#E9EDFA",
+          }}
+        >
+          <p
+            style={{
+              textAlign: "center",
+              color: "#373F60",
+              fontSize: "20px",
+              lineHeight: "28px",
+            }}
+          >
+            음식서비스•
+            <br />
+            식품가공 ISC
+          </p>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            boxSizing: "border-box",
+            alignItems: "center",
+            width: "200px",
+            height: "200px",
+            borderRadius: "100px",
+            border: "1px solid #D8DDE5",
+            background: "#E9EDFA",
+          }}
+        >
+          <p
+            style={{
+              textAlign: "center",
+              color: "#373F60",
+              fontSize: "20px",
+              lineHeight: "28px",
+            }}
+          >
+            연구 및 출판
+          </p>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            boxSizing: "border-box",
+            alignItems: "center",
+            width: "200px",
+            height: "200px",
+            borderRadius: "100px",
+            border: "1px solid #D8DDE5",
+            background: "#E9EDFA",
+          }}
+        >
+          <p
+            style={{
+              textAlign: "center",
+              color: "#373F60",
+              fontSize: "20px",
+              lineHeight: "28px",
+            }}
+          >
+            정부•지자체
+            <br />
+            연구학술용역
+          </p>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            boxSizing: "border-box",
+            alignItems: "center",
+            width: "200px",
+            height: "200px",
+            borderRadius: "100px",
+            border: "1px solid #D8DDE5",
+            background: "#E9EDFA",
+          }}
+        >
+          <p
+            style={{
+              textAlign: "center",
+              color: "#373F60",
+              fontSize: "20px",
+              lineHeight: "28px",
+            }}
+          >
+            교육•훈련
+          </p>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            boxSizing: "border-box",
+            alignItems: "center",
+            width: "200px",
+            height: "200px",
+            borderRadius: "100px",
+            border: "1px solid #D8DDE5",
+            background: "#E9EDFA",
+          }}
+        >
+          <p
+            style={{
+              textAlign: "center",
+              color: "#373F60",
+              fontSize: "20px",
+              lineHeight: "28px",
+            }}
+          >
+            민간자격
+          </p>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            boxSizing: "border-box",
+            alignItems: "center",
+            width: "200px",
+            height: "200px",
+            borderRadius: "100px",
+            border: "1px solid #D8DDE5",
+            background: "#E9EDFA",
+          }}
+        >
+          <p
+            style={{
+              textAlign: "center",
+              color: "#373F60",
+              fontSize: "20px",
+              lineHeight: "28px",
+            }}
+          >
+            지식네트워킹
+          </p>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            boxSizing: "border-box",
+            alignItems: "center",
+            width: "200px",
+            height: "200px",
+            borderRadius: "100px",
+            border: "1px solid #D8DDE5",
+            background: "#E9EDFA",
+          }}
+        >
+          <p
+            style={{
+              textAlign: "center",
+              color: "#373F60",
+              fontSize: "20px",
+              lineHeight: "28px",
+            }}
+          >
+            외식산업 정보
+            <br />
+            인프라 구축
+          </p>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            boxSizing: "border-box",
+            alignItems: "center",
+            width: "200px",
+            height: "200px",
+            borderRadius: "100px",
+            border: "1px solid #D8DDE5",
+            background: "#E9EDFA",
+          }}
+        >
+          <p
+            style={{
+              textAlign: "center",
+              color: "#373F60",
+              fontSize: "20px",
+              lineHeight: "28px",
+            }}
+          >
+            한국외식산업
+            <br />
+            통계연감
+          </p>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            boxSizing: "border-box",
+            alignItems: "center",
+            width: "200px",
+            height: "200px",
+            borderRadius: "100px",
+            border: "1px solid #D8DDE5",
+            background: "#E9EDFA",
+          }}
+        >
+          <p
+            style={{
+              textAlign: "center",
+              color: "#373F60",
+              fontSize: "20px",
+              lineHeight: "28px",
+            }}
+          >
+            외식종합컨설팅
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
