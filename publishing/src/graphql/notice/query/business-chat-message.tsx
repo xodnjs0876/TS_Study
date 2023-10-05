ec0f4baa7f8a4f260974b2d74a1e742323f9c08b
xodnjs0876 <taewon.kim@wavle.io>
2023-10-05 17:45:04 +0900

    채팅 메세지 쿼리

---
+++ publishing/src/graphql/notice/query/business-chat-message.tsx
@@ -0,0 +1,50 @@
+import { gql } from "@apollo/client";
+
+const BUSINESSCHATMESSAGE = gql`
+  query ($channelId: ID!, $sort: [BusinessChatMessageSortInput!]) {
+    businessChatMessages(channelId: $channelId, sort: $sort) {
+      totalCount
+      edges {
+        node {
+          id
+          message
+          type
+          unreadUserCount
+          createdAt
+          author {
+            name
+            id
+          }
+          payload {
+            ... on ChatMessageFileTypePayload {
+              filename
+              id
+              size
+              url
+            }
+            ... on ChatMessageLinkTypePayload {
+              link
+            }
+          }
+          channel {
+            secondhand {
+              author {
+                id
+                name
+              }
+            }
+            participants {
+              isMine
+              user {
+                id
+                name
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default BUSINESSCHATMESSAGE;
