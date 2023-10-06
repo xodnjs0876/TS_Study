b5d3f0951ab1d84ac25f9f6d39bb8b7a09cf011f
xodnjs0876 <taewon.kim@wavle.io>
2023-10-06 17:32:35 +0900

    graphql 적용

--- publishing/src/index.tsx
+++ publishing/src/index.tsx
@@ -3,12 +3,53 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-import { GoogleOAuthProvider } from "@react-oauth/google";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+  split,
+} from "@apollo/client";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { createClient } from "graphql-ws";
 import "./index.css";
+import { getMainDefinition } from "@apollo/client/utilities";
 
-export const client = new ApolloClient({
+localStorage.setItem(
+  "accessToken",
+  "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoiQUNDRVNTIiwiaWQiOiIxODlmZDc1Yy01MWYyLTRlOWUtOTcyMC0zYjk4ZGQ1Zjk5NjkiLCJwYXJlbnQiOiJmZWQ3OGQ0Zi1jM2MzLTRhYTMtOTdkMy0yNDUzMWU2Zjg2NzgiLCJpYXQiOjE2OTU3Nzg4MDcsImV4cCI6OTQ3MTc3ODgwNywiaXNzIjoia19maXJpIiwic3ViIjoiMTg5ZmQ3NWMtNTFmMi00ZTllLTk3MjAtM2I5OGRkNWY5OTY5IiwianRpIjoiMjc1MGUzMWUtMzc3MC00NDQxLWFjYmItZWRmMTJhZDVkODBiIn0.Q5puQEyGSWqcD0HbDqDMLnw0ggsGZuY96XU9eQa-7Z8gQXaUDd8ctfrzFgJeXr-eRaq6TuWMZiTtXqtUxsvDHw"
+);
+
+const httpLink = new HttpLink({
   uri: "http://211.110.139.183:5900/graphql",
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+  },
+});
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: "ws://211.110.139.183:5900/graphql",
+    connectionParams: {
+      authToken: localStorage.getItem("accessToken"),
+    },
+  })
+);
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  httpLink
+);
+export const client = new ApolloClient({
+  link: splitLink,
+  // headers:{
+  //   Authorization:"Bearer accesstoken"
+  // },
   cache: new InMemoryCache({}),
 });
 
@@ -16,14 +57,11 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-const googleClientId = process.env.REACT_APP_GOOGLE_CLIENT_ID ?? "";
 root.render(
   <ApolloProvider client={client}>
-    <GoogleOAuthProvider clientId={googleClientId}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </GoogleOAuthProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </ApolloProvider>
 );
 
