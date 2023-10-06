e270eb60ad044580be820de5e5422a2562112d57
xodnjs0876 <taewon.kim@wavle.io>
2023-10-06 17:32:14 +0900

    날짜 형식 변환 컴포넌트

--- publishing/src/components/notification/format-date-time.tsx
+++ publishing/src/components/notification/format-date-time.tsx
@@ -1,7 +1,14 @@
 import { DateTime } from "luxon";
 
-const formatDateTime = (createdAt: string) => {
-  return DateTime.fromISO(createdAt).toFormat("yyyy-MM-dd");
+const formatDateTime = (date: string | DateTime) => {
+  let dateTime: DateTime;
+  if (date instanceof DateTime) {
+    dateTime = date;
+  } else {
+    dateTime = DateTime.fromISO(date);
+  }
+
+  return dateTime.setLocale("ko-KR").toFormat("MM월 dd일 a h:mm");
 };
 
 export default formatDateTime;
