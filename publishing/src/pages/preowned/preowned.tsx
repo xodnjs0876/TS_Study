e9f0aa549c9723055a149c4d82992a6498365778
xodnjs0876 <taewon.kim@wavle.io>
2023-10-13 17:43:03 +0900

    코드 정리 및 모바일 페이지 작업

--- publishing/src/pages/preowned/preowned.tsx
+++ publishing/src/pages/preowned/preowned.tsx
@@ -1,19 +1,24 @@
-import React, { useCallback, useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { styled } from "styled-components";
 import { useMutation, useQuery } from "@apollo/client";
 import MYBUSINESSCHATCHANNELS from "../../graphql/preowned/query/my-business-chat-channels";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import ChatDetail from "./[id]";
-import { DateTime } from "luxon";
 import READCHATMESSAGE from "../../graphql/preowned/mutation/read-business-chat-message";
 import BUSINESSCHATCHANNELS from "../../graphql/preowned/query/business-chat-channel";
-import BUSINESSCHATMESSAGE from "../../graphql/preowned/query/business-chat-message";
+import { formatDate } from "../../function/format-date";
+import Spinner from "../../assets/img/spinner.gif";
 
 interface ChatChannel {
   id: string;
   unreadMessageCount: number;
   updatedAt: string;
   secondhand: {
+    images: [
+      {
+        url: string;
+      }
+    ];
     author: {
       id: string;
       name: string;
@@ -28,24 +33,11 @@ interface ChatChannelEdge {
   node: ChatChannel;
 }
 export default function Preowned() {
-  const convertDate = useCallback((date: string | undefined) => {
-    if (date === undefined) return "-";
-    const inputDateTime = DateTime.fromISO(date);
-    const now = DateTime.now();
-    const day = now.startOf("day");
-    const yesterday = now.minus({ days: 1 }).startOf("day");
+  const navigate = useNavigate();
+  const params = useParams();
 
-    if (inputDateTime >= day && inputDateTime <= day.endOf("day")) {
-      return inputDateTime.setLocale("ko").toFormat("a h:mm");
-    } else if (
-      inputDateTime >= yesterday.startOf("day") &&
-      inputDateTime <= yesterday.endOf("day")
-    ) {
-      return "어제";
-    } else {
-      return inputDateTime.toFormat("M월 d일");
-    }
-  }, []);
+  const [channelId, setBtnActive] = useState<string>("");
+
   const { data, loading } = useQuery(MYBUSINESSCHATCHANNELS, {
     variables: {
       sort: {
@@ -56,20 +48,23 @@ export default function Preowned() {
     },
   });
   const [readMessage] = useMutation(READCHATMESSAGE);
-  const navigate = useNavigate();
-  const [btnActive, setBtnActive] = useState<number>(0);
-  const toggleActive = (e: number) => {
-    setBtnActive((prev) => {
-      return e;
-    });
-  };
 
   const edges = useMemo(() => {
-    return data?.myBusinessChatChannels?.edges;
+    return data?.myBusinessChatChannels?.edges ?? [];
   }, [data]);
 
+  useEffect(() => {
+    if (!params.id) {
+      setBtnActive("");
+    }
+  }, [params.id]);
+
   if (loading) {
-    return <div>로딩중</div>;
+    return (
+      <Loading>
+        <img src={Spinner} alt="spin" />
+      </Loading>
+    );
   }
   return (
     <Layout>
@@ -78,22 +73,27 @@ export default function Preowned() {
         <h1>채팅</h1>
       </TextHeader>
       <Chat>
-        <ActiveChattingList>
+        <ActiveChattingList id={channelId}>
           <ListTitle>
             <h2>진행중인 채팅 목록</h2>
             <span>{data?.myBusinessChatChannels?.totalCount}</span>
           </ListTitle>
           {edges &&
             edges.map((edge: ChatChannelEdge, i: number) => {
+              const item = edge.node;
+              const id = item.id;
+
+              if (item === undefined) return <></>;
+
               return (
                 <List
-                  className={"list" + (i === btnActive ? " active" : "")}
+                  className={"list" + (id === channelId ? " active" : "")}
                   key={i}
                   onClick={() => {
-                    toggleActive(i);
-                    navigate(`/preowned?id=${edge.node.id}`);
+                    setBtnActive(id);
+                    navigate(`/preowned/${id}`);
                     readMessage({
-                      variables: { channelId: edge.node.id },
+                      variables: { channelId: id },
                       refetchQueries: [
                         MYBUSINESSCHATCHANNELS,
                         BUSINESSCHATCHANNELS,
@@ -103,27 +103,36 @@ export default function Preowned() {
                 >
                   <Content>
                     <ChattingListTitle>
-                      <h3>{edge.node.secondhand.author.name}</h3>
-                      <span>{convertDate(edge.node.updatedAt)}</span>
+                      <h3>{item.secondhand?.author.name}</h3>
+                      <span>{formatDate(item.updatedAt, false)}</span>
                     </ChattingListTitle>
                     <ChatContent>
-                      <p>{edge.node.lastMessage.message}</p>
-                      {edge.node.unreadMessageCount !== 0 && (
+                      <p>{item.lastMessage?.message}</p>
+                      {item.unreadMessageCount !== 0 && (
                         <div className="unRead">
-                          <span>{edge.node.unreadMessageCount}</span>
+                          <span>{item.unreadMessageCount}</span>
                         </div>
                       )}
                     </ChatContent>
                   </Content>
-                  <div className="img">
-                    <img src="" alt="" />
-                    이미지
+                  <div>
+                    <img src={item.secondhand?.images[0].url} alt="img" />
                   </div>
                 </List>
               );
             })}
         </ActiveChattingList>
-        <ChatDetail />
+        <Detail id={channelId}>
+          {channelId ? (
+            <ChatDetail />
+          ) : (
+            <NotChatting>
+              <Text>
+                <span>진행중인 채팅방이 없음</span>
+              </Text>
+            </NotChatting>
+          )}
+        </Detail>
       </Chat>
     </Layout>
   );
@@ -132,6 +141,12 @@ const Layout = styled.div`
   width: 1280px;
   margin: auto;
   background-color: #f9f9f9;
+
+  @media screen and (max-width: 600px) {
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+  }
 `;
 const TextHeader = styled.div`
   display: flex;
@@ -147,7 +162,7 @@ const TextHeader = styled.div`
     font-size: 18px;
     font-style: normal;
     font-weight: 500;
-    line-height: 18px; /* 100% */
+    line-height: 18px;
   }
   h1 {
     color: #333;
@@ -157,7 +172,25 @@ const TextHeader = styled.div`
     font-size: 30px;
     font-style: normal;
     font-weight: 700;
-    line-height: 30px; /* 100% */
+    line-height: 30px;
+  }
+  @media screen and (max-width: 600px) {
+    padding: 109px 0 78px 75px;
+    display: flex;
+    align-items: flex-start;
+    h3 {
+      color: #444;
+      font-feature-settings: "clig" off, "liga" off;
+      font-family: Pretendard;
+      font-size: 24px;
+      font-style: normal;
+      font-weight: 700;
+      line-height: 24px;
+      text-align: left;
+    }
+    h1 {
+      display: none;
+    }
   }
 `;
 
@@ -169,13 +202,13 @@ const Chat = styled.div`
   }
 `;
 
-const ActiveChattingList = styled.div`
+const ActiveChattingList = styled.div<{ id: string }>`
   width: 420px;
   border-radius: 10px;
   border: 1px solid #d8dde5;
   background: #fff;
   height: 800px;
-  overflow: auto;
+  overflow: scroll;
   &::-webkit-scrollbar {
     width: 5px;
     height: 2px;
@@ -184,6 +217,14 @@ const ActiveChattingList = styled.div`
   &::-webkit-scrollbar-thumb {
     background: rgba(0, 0, 0, 0.3);
     border-radius: 6px;
+  }
+
+  @media screen and (max-width: 600px) {
+    display: ${(props) => (props.id ? "none" : "")};
+    width: 100vw;
+    height: 100vh;
+    border-radius: 0px;
+    justify-content: center;
   }
 `;
 
@@ -211,20 +252,49 @@ const ListTitle = styled.div`
     font-weight: 700;
     line-height: 30px; /* 166.667% */
   }
+
+  @media screen and (max-width: 600px) {
+    padding: 17px 0 17px 20px;
+    h2 {
+      color: #333;
+      font-feature-settings: "clig" off, "liga" off;
+      font-family: Pretendard;
+      font-size: 16px;
+      font-style: normal;
+      font-weight: 700;
+      line-height: 16px; /* 100% */
+    }
+    span {
+      color: var(--unnamed, #193dd0);
+      font-feature-settings: "clig" off, "liga" off;
+      font-family: Pretendard;
+      font-size: 16px;
+      font-style: normal;
+      font-weight: 700;
+      line-height: 16px; /* 100% */
+    }
+  }
 `;
 
 const List = styled.div`
   display: flex;
-  align-items: center;
+  align-items: flex-start;
   gap: 10px;
-  padding: 20px;
+  padding: 21px;
   border-bottom: 1px solid #d8dde5;
   cursor: pointer;
-  .img {
+  img {
+    border: 1px solid #d8dde5;
     width: 60px;
     height: 60px;
     border-radius: 5px;
-    border: 1px solid #d8dde5;
+    box-sizing: border-box;
+  }
+
+  @media screen and (max-width: 600px) {
+    padding: 20px;
+    margin: auto;
+    justify-content: center;
   }
 `;
 
@@ -232,7 +302,7 @@ const Content = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-  gap: 12px;
+  gap: 20px;
 `;
 
 const ChattingListTitle = styled.div`
@@ -241,13 +311,14 @@ const ChattingListTitle = styled.div`
   justify-content: space-between;
   align-items: center;
   flex-shrink: 0;
+  padding-top: 5px;
   h3 {
     color: #333;
     font-family: Pretendard;
     font-size: 14px;
     font-style: normal;
     font-weight: 400;
-    line-height: 14px; /* 100% */
+    line-height: 14px;
   }
   span {
     color: #666;
@@ -256,7 +327,10 @@ const ChattingListTitle = styled.div`
     font-size: 14px;
     font-style: normal;
     font-weight: 400;
-    line-height: 14px; /* 100% */
+    line-height: 14px;
+  }
+  @media screen and (max-width: 600px) {
+    width: 265px;
   }
 `;
 const ChatContent = styled.div`
@@ -292,6 +366,47 @@ const ChatContent = styled.div`
     font-size: 14px;
     font-style: normal;
     font-weight: 400;
-    line-height: 14px; /* 100% */
+    line-height: 14px;
+  }
+  @media screen and (max-width: 600px) {
+    width: 265px;
+  }
+`;
+const NotChatting = styled.div`
+  width: 800px;
+  height: 800px;
+  flex-shrink: 0;
+  border-radius: 10px;
+  border: 1px solid #d8dde5;
+  background: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Text = styled.div`
+  span {
+    font-family: Pretendard;
+    font-size: 30px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 14px;
+  }
+`;
+
+const Detail = styled.div<{ id: string }>`
+  @media screen and (max-width: 600px) {
+    display: ${(props) => (props.id ? "" : "none")};
+  }
+`;
+
+const Loading = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  @media screen and (max-width: 600px) {
+    width: 100vw;
+    height: 100vw;
   }
 `;
