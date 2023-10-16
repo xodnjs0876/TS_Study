35053721c7f7f28d51a3994ff700fbadb7cbacdd
xodnjs0876 <taewon.kim@wavle.io>
2023-10-16 16:26:57 +0900

    라우터 구조 변경

--- publishing/src/App.tsx
+++ publishing/src/App.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { styled } from "styled-components";
 import { Route, Routes } from "react-router-dom";
 import Login from "./pages/auth/login";
 import { AuthProvider } from "./components/auth/provider";
 import Preowned from "./pages/preowned/preowned";
 import ChatDetail from "./pages/preowned/[id]";
+import { Mobile, Pc } from "./responsive";
 // import NotisDetail from "./pages/notis/detail/notice-detail";
 // import Notification from "./pages/notis/notification";
 // import NaverRedirect from "./pages/auth/naver";
@@ -15,12 +16,19 @@ export default function App() {
   return (
     <AuthProvider>
       <Layout>
-        <Routes>
-          <Route path="/" Component={Login} />
-          <Route path="/preowned/" Component={Preowned}>
+        <Mobile>
+          <Routes>
+            <Route path="/preowned/" Component={Preowned} />
             <Route path="/preowned/:id" Component={ChatDetail} />
-          </Route>
-        </Routes>
+          </Routes>
+        </Mobile>
+        <Pc>
+          <Routes>
+            <Route path="/preowned/" Component={Preowned}>
+              <Route path="/preowned/:id" Component={ChatDetail} />
+            </Route>
+          </Routes>
+        </Pc>
         {/* <Routes>
             <Route Component={MainLayout}>
               <Route path="/" Component={Notification} />
